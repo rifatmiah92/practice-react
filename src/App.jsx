36b8c54eb32bd683d28148af6f9b6c441fae84f8
rifatmiah92 <hrrifat92@gmail.com>
2025-09-21 16:33:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+
+import './App.css'
+import Countries from './component/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+     
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>My name is Abraham</h1>
+        </div>
+    );
+};
+
+export default Countries;
